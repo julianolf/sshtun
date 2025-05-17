78130ec0bc573a06baa965ce90610196bdfc4d18
Juliano Fernandes <julianofernandes@gmail.com>
2025-05-17 20:35:19 -0300

    Improved the installation script by outputting messages about the ongoing process.

--- install.sh
+++ install.sh
@@ -2,9 +2,11 @@
 
 set -euo pipefail
 
-src=https://raw.githubusercontent.com/julianolf/sshtun/refs/heads/main/sshtun.sh
+src=https://raw.githubusercontent.com/julianolf/sshtun/main/sshtun.sh
 dst=/usr/local/bin/sshtun
 
 sudo -v
+echo "Downloading sshtun..."
 sudo curl -sSfL --output "$dst" "$src"
 sudo chmod +x "$dst"
+echo "Installation complete."
